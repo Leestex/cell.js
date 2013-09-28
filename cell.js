2509ab9e2fdf63380a1cd753781bd6781a514f7a
Nazar Kuzmenko <nazar@streamcrease.com>
2013-09-28 11:30:49 +0300

    Basic functionality

---
+++ cell.js
@@ -0,0 +1,73 @@
+;(function ( window, undefined ) {
+
+    var Cell = function Cell ( options ) {
+
+        this._value = undefined;
+        this.deps = [];
+        this.usage = [];
+
+        if ( typeof options[ 0 ] == 'function' ) {
+            this.formula = options[ 0 ];
+            this.usage = options.slice( 1 );
+            for ( var i = 0; i < this.usage.length; i++ ) {
+                this.usage[ i ].deps.push( this );
+            }
+        } else {
+            this._value = options[ 0 ];
+        }
+
+        this.calculate();
+
+    };
+
+    Cell.prototype.set = function ( val ) {
+
+        this._value = val;
+        this.updateDeps();
+
+    };
+
+    Cell.prototype.value = function () {
+
+        return this._value;
+
+    };
+
+    Cell.prototype.calculate = function () {
+
+        if ( this.formula ) {
+            this._value = this.formula.apply( this, this.getUsageValues() );
+            this.updateDeps();
+        }
+
+    };
+
+    Cell.prototype.getUsageValues = function () {
+
+        var result = [];
+
+        if ( this.usage && this.usage.length ) {
+            for ( var i = 0; i < this.usage.length; i++ ) {
+                result[ i ] = this.usage[ i ].value();
+            }
+        }
+
+        return result;
+
+    };
+
+    Cell.prototype.updateDeps = function () {
+
+        for ( var i = 0; i < this.deps.length; i++ ) {
+            this.deps[ i ].calculate();
+        }
+
+    };
+
+    var cell = function () {
+        return new Cell( Array.prototype.slice.call( arguments, 0 ) );
+    };
+
+    window.cell = cell;
+
+})( window );
